699bcb2f31c493c55537e7c377ae3610ffeef374
Rory McCune <rorym@mccune.org.uk>
2023-01-22 10:44:52 +0000

    add HTML styling

--- pkg/eathar/reporting.go
+++ pkg/eathar/reporting.go
@@ -16,6 +16,57 @@ import (
 	v1 "k8s.io/api/rbac/v1"
 )
 
+var style string = ` <style>
+body {
+	font: normal 14px;
+	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+	color: #C41230;
+	background: #FFFFFF;
+}
+#kubernetes-analyzer {
+	font-weight: bold;
+	font-size: 48px;
+	color: #C41230;
+}
+.master-node, .worker-node, .vuln-node {
+	background: #F5F5F5;
+	border: 1px solid black;
+	padding-left: 6px;
+}
+#api-server-results {
+	font-weight: italic;
+	font-size: 36px;
+	color: #C41230;
+}
+table, th, td {
+	border-collapse: collapse;
+	border: 1px solid black;
+}
+th {
+ font: bold 11px;
+ color: #C41230;
+ background: #999999;
+ letter-spacing: 2px;
+ text-transform: uppercase;
+ text-align: left;
+ padding: 6px 6px 6px 12px;
+}
+td {
+background: #FFFFFF;
+padding: 6px 6px 6px 12px;
+color: #333333;
+}
+.container{
+	display: flex;
+} 
+.fixed{
+	width: 300px;
+}
+.flex-item{
+	flex-grow: 1;
+}
+</style>`
+
 func ReportPSS(f []Finding, options *pflag.FlagSet, check string) {
 	jsonrep, _ := options.GetBool("jsonrep")
 	htmlrep, _ := options.GetBool("htmlrep")
@@ -42,7 +93,7 @@ func ReportPSS(f []Finding, options *pflag.FlagSet, check string) {
 		} else {
 			rep = os.Stdout
 		}
-		fmt.Fprintf(rep, "<head><title>Findings for the %s check</title></head><body>", check)
+		fmt.Fprintf(rep, "<html><head>%s<title>Findings for the %s check</title></head><body>", style, check)
 		fmt.Fprintf(rep, "<h1>Findings for the %s check</h1>", check)
 		if f != nil {
 			fmt.Fprintln(rep, "<table>")
@@ -129,6 +180,7 @@ func ReportPSS(f []Finding, options *pflag.FlagSet, check string) {
 
 func ReportImage(f []string, options *pflag.FlagSet, check string) {
 	jsonrep, _ := options.GetBool("jsonrep")
+	htmlrep, _ := options.GetBool("htmlrep")
 	file, _ := options.GetString("file")
 
 	var rep *os.File
@@ -146,6 +198,21 @@ func ReportImage(f []string, options *pflag.FlagSet, check string) {
 			}
 			fmt.Fprintln(rep, string(js))
 		}
+	case htmlrep:
+		if file != "" {
+			rep, _ = os.OpenFile(file+".html", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		} else {
+			rep = os.Stdout
+		}
+		fmt.Fprintf(rep, "<html><head>%s<title>Image List</title></head><body><table><tr><th>Image</th></tr>", style)
+		if f != nil {
+			for _, i := range f {
+				fmt.Fprintf(rep, "<tr><td>%s</td></tr>", i)
+			}
+		} else {
+			fmt.Fprintln(rep, "<tr><td>No findings</td></tr>")
+		}
+		fmt.Fprintln(rep, "</table></body></html>")
 	default:
 		if file != "" {
 			rep, _ = os.OpenFile(file+".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -166,6 +233,7 @@ func ReportImage(f []string, options *pflag.FlagSet, check string) {
 
 func ReportRBAC(f v1.ClusterRoleBindingList, options *pflag.FlagSet, check string) {
 	jsonrep, _ := options.GetBool("jsonrep")
+	htmlrep, _ := options.GetBool("htmlrep")
 	file, _ := options.GetString("file")
 
 	var rep *os.File
@@ -182,6 +250,30 @@ func ReportRBAC(f v1.ClusterRoleBindingList, options *pflag.FlagSet, check strin
 				log.Print(err)
 			}
 			fmt.Fprintln(rep, string(js))
+		}
+	case htmlrep:
+		if file != "" {
+			rep, _ = os.OpenFile(file+".html", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		} else {
+			rep = os.Stdout
+		}
+		fmt.Fprintf(rep, "<html><head>%s<title>RBAC Report</title></head><body>", style)
+		if f.Items != nil {
+			fmt.Fprintf(rep, "<table><tr><th>ClusterRoleBinding</th><th>Subjects</th><th>Role Ref</th></tr>")
+			for _, i := range f.Items {
+				fmt.Fprintf(rep, "<tr><td>%s</td>", i.Name)
+				for _, s := range i.Subjects {
+					if s.Kind == "ServiceAccount" {
+						fmt.Fprintf(rep, "<td>Kind: %s, Name: %s, Namespace: %s</td>", s.Kind, s.Name, s.Namespace)
+					} else {
+						fmt.Fprintf(rep, "<td>Kind: %s, Name: %s</td>", s.Kind, s.Name)
+					}
+				}
+				fmt.Fprintf(rep, "<td>Kind: %s, Name: %s</td></tr>", i.RoleRef.Kind, i.RoleRef.Name)
+			}
+			fmt.Fprintln(rep, "</table></body></html>")
+		} else {
+			fmt.Fprintln(rep, "No findings!")
 		}
 	default:
 		if file != "" {
